ce92fc66fcfff23aecf641c5e6e7bff589249b5a
ysh-nh <yuushin.n999n@gmail.com>
2024-03-23 11:04:14 +0900

    add Post function(edit update destroy)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[edit update destroy]
   
   def index
     @posts = Post.all
@@ -18,10 +19,30 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:title, :content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   # get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  resources :posts, only: %i[index new create]
+  resources :posts, only: %i[index new create edit update destroy]
 end
